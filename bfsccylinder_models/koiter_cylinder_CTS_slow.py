BUG: Koiter buckling mode normalization based on displacement, TST: updated accordingly

bfsccylinder_models/koiter_cylinder_CTS_slow.py
@@ -24,8 +24,8 @@ def fkoiter_cylinder_CTS_circum(L, R, rCTS, nxt, ny, E11, E22, nu12, G12, rho,
         h_tow, param_n, c2_ratio, thetadeg_c1, thetadeg_c2,
         ny_nx_aspect_ratio=1, cg_x0=None,
         idealistic_CTS=False, mesh_only=False, nint=4, num_eigvals=2,
-        koiter_num_modes=1, load=1000, NLprebuck=False,
-        max_ny_nx_aspect_ratio=2):
+        koiter_num_modes=1, Nxxunit=1., NLprebuck=False,
+        max_ny_nx_aspect_ratio=2, zero_offset=False):
 
     circ = 2*np.pi*R
     out = {}
@@ -214,6 +214,8 @@ def fkoiter_cylinder_CTS_circum(L, R, rCTS, nxt, ny, E11, E22, nu12, G12, rho,
             plyts = (plyt_local, plyt_local)
 
             offset = sum(plyts)/2.
+            if zero_offset:
+                offset = 0
             prop = laminated_plate(stack=stack, plyts=plyts, laminaprop=laminaprop, offset=offset, rho=rho)
             for j in range(nint):
                 wj = weights[j]
@@ -258,7 +260,6 @@ def fkoiter_cylinder_CTS_circum(L, R, rCTS, nxt, ny, E11, E22, nu12, G12, rho,
     out['thetadegavg_elements'] = thetadegavg_elements
     out['havg_elements'] = havg_elements
     out['havg'] = havg
-    out['elements'] = elements
 
     if mesh_only:
         return out
@@ -289,12 +290,38 @@ def fkoiter_cylinder_CTS_circum(L, R, rCTS, nxt, ny, E11, E22, nu12, G12, rho,
     print('# starting static analysis')
 
     # axially compressive load applied at x=0 and x=L
-    checkTopEdge = isclose(x, L)
-    checkBottomEdge = isclose(x, 0)
     fext = np.zeros(N)
-    fext[0::DOF][checkBottomEdge] = +load/ny
-    assert isclose(fext.sum(), load)
-    fext[0::DOF][checkTopEdge] = -load/ny
+    # applying load
+    for elem in elements:
+        pos1 = nid_pos[elem.n1]
+        pos2 = nid_pos[elem.n2]
+        pos3 = nid_pos[elem.n3]
+        pos4 = nid_pos[elem.n4]
+        if isclose(x[pos3], L):
+            Nxx = -Nxxunit
+            xi = +1
+        elif isclose(x[pos1], 0):
+            Nxx = +Nxxunit
+            xi = -1
+        else:
+            continue
+        lex = elem.lex
+        ley = elem.ley
+        indices = []
+        c1 = DOF*pos1
+        c2 = DOF*pos2
+        c3 = DOF*pos3
+        c4 = DOF*pos4
+        cs = [c1, c2, c3, c4]
+        for ci in cs:
+            for i in range(DOF):
+                indices.append(ci + i)
+        fe = np.zeros(num_nodes*DOF, dtype=float)
+        for j in range(nint):
+            eta = points[j]
+            elem.update_Nu(xi, eta)
+            fe += ley/2.*weights[j]*elem.Nu*Nxx
+        fext[indices] += fe
     assert isclose(fext.sum(), 0)
 
     # sub-matrices corresponding to unknown DOFs
@@ -370,7 +397,6 @@ def fkoiter_cylinder_CTS_circum(L, R, rCTS, nxt, ny, E11, E22, nu12, G12, rho,
             ui = u.copy()
         u0 = u.copy()
 
-
         KCNLv *= 0
         for elem in elements:
             update_KCNL(u0, elem, points, weights, KCNLr, KCNLc, KCNLv)
@@ -396,16 +422,18 @@ def fkoiter_cylinder_CTS_circum(L, R, rCTS, nxt, ny, E11, E22, nu12, G12, rho,
     KGuu = KG[bu, :][:, bu]
 
     print('# starting eigenvalue analysis')
-    eigvals, eigvecsu = eigsh(A=KCuu, k=num_eigvals, which='SM', M=KGuu,
-            tol=1e-6, sigma=1., mode='buckling')
-    load_mult = -eigvals
+    #eigvals, eigvecsu = eigsh(A=KCuu, k=num_eigvals, which='SM', M=KGuu,
+            #tol=1e-8, sigma=1., mode='buckling')
+    #load_mult = eigvals
+    eigvals, eigvecsu = eigsh(A=KGuu, k=num_eigvals, which='LM', M=KCuu,
+            tol=1e-6)
+    load_mult = -1/eigvals
     print('# finished eigenvalue analysis')
 
-    Pcr = load_mult[0]*load
-    print('# eigvals', load_mult)
+    Pcr = load_mult[0]*Nxxunit*circ
+    print('# load_mult', load_mult)
     print('# critical buckling load', Pcr)
 
-    out['P0'] = load
     out['Pcr'] = Pcr
     out['cg_x0'] = cg_x0
     out['eigvals'] = eigvals
@@ -426,21 +454,15 @@ def fkoiter_cylinder_CTS_circum(L, R, rCTS, nxt, ny, E11, E22, nu12, G12, rho,
         lambda_a[modei] = load_mult[modei]
 
     es = partial(np.einsum, optimize='greedy', casting='no')
-    #from opt_einsum import contract
-    #es = partial(contract)
 
     #NOTE making the maximum amplitude of the eigenmode equal to h
     #normalizing amplitude of eigenvector according to shell thickness
     ua = {}
     for modei in range(koiter_num_modes):
         ua[modei] = eigvecs[:, modei].copy()
-        #if isclose(abs(ua[modei][6::DOF].max()), abs(ua[modei][6::DOF].min())):
-            #ua[modei] /= abs(ua[modei][6::DOF].max())
-        #elif abs(ua[modei][6::DOF].max()) >= abs(ua[modei][6::DOF].min()):
-            #ua[modei] /= ua[modei][6::DOF].max()
-        #else:
-            #ua[modei] /= ua[modei][6::DOF].min()
-        ua[modei] /= ua[modei].max()
+        #NOTE normalizing as Abaqus does, assuming nonzero translations
+        ampl = np.sqrt(ua[modei][0::DOF]**2 + ua[modei][3::DOF]**2 + ua[modei][6::DOF]**2).max()
+        ua[modei] /= ampl
         ua[modei] *= havg
 
     phi4 = defaultdict(lambda: 0)

bfsccylinder_models/koiter_cylinder_newton_raphson.py
@@ -21,7 +21,7 @@ num_nodes = 4
 
 
 def fkoiter_cyl_SS3(L, R, nx, ny, prop, cg_x0=None, nint=4,
-        num_eigvals=2, koiter_num_modes=1, load=1000, NLprebuck=False):
+        num_eigvals=2, koiter_num_modes=1, Nxxunit=1., NLprebuck=False):
 
     circ = 2*np.pi*R
     out = {}
@@ -104,7 +104,6 @@ def fkoiter_cyl_SS3(L, R, nx, ny, prop, cg_x0=None, nint=4,
     out['volume'] = volume
     out['mass'] = mass
     out['havg'] = havg
-    out['elements'] = elements
     KC0r = np.zeros(KC0_SPARSE_SIZE*num_elements, dtype=INT)
     KC0c = np.zeros(KC0_SPARSE_SIZE*num_elements, dtype=INT)
     KC0v = np.zeros(KC0_SPARSE_SIZE*num_elements, dtype=DOUBLE)
@@ -131,12 +130,38 @@ def fkoiter_cyl_SS3(L, R, nx, ny, prop, cg_x0=None, nint=4,
     print('# starting static analysis')
 
     # axially compressive load applied at x=0 and x=L
-    checkTopEdge = isclose(x, L)
-    checkBottomEdge = isclose(x, 0)
     fext = np.zeros(N)
-    fext[0::DOF][checkBottomEdge] = +load/ny
-    assert isclose(fext.sum(), load)
-    fext[0::DOF][checkTopEdge] = -load/ny
+    # applying load
+    for elem in elements:
+        pos1 = nid_pos[elem.n1]
+        pos2 = nid_pos[elem.n2]
+        pos3 = nid_pos[elem.n3]
+        pos4 = nid_pos[elem.n4]
+        if isclose(x[pos3], L):
+            Nxx = -Nxxunit
+            xi = +1
+        elif isclose(x[pos1], 0):
+            Nxx = +Nxxunit
+            xi = -1
+        else:
+            continue
+        lex = elem.lex
+        ley = elem.ley
+        indices = []
+        c1 = DOF*pos1
+        c2 = DOF*pos2
+        c3 = DOF*pos3
+        c4 = DOF*pos4
+        cs = [c1, c2, c3, c4]
+        for ci in cs:
+            for i in range(DOF):
+                indices.append(ci + i)
+        fe = np.zeros(num_nodes*DOF, dtype=float)
+        for j in range(nint):
+            eta = points[j]
+            elem.update_Nu(xi, eta)
+            fe += ley/2.*weights[j]*elem.Nu*Nxx
+        fext[indices] += fe
     assert isclose(fext.sum(), 0)
 
     # sub-matrices corresponding to unknown DOFs
@@ -238,16 +263,18 @@ def fkoiter_cyl_SS3(L, R, nx, ny, prop, cg_x0=None, nint=4,
     KGuu = KG[bu, :][:, bu]
 
     print('# starting eigenvalue analysis')
-    eigvals, eigvecsu = eigsh(A=KCuu, k=num_eigvals, which='SM', M=KGuu,
-            tol=1e-6, sigma=1., mode='buckling')
-    load_mult = -eigvals
+    #eigvals, eigvecsu = eigsh(A=KCuu, k=num_eigvals, which='SM', M=KGuu,
+            #tol=1e-8, sigma=1., mode='buckling')
+    #load_mult = eigvals
+    eigvals, eigvecsu = eigsh(A=KGuu, k=num_eigvals, which='LM', M=KCuu,
+            tol=1e-6)
+    load_mult = -1/eigvals
     print('# finished eigenvalue analysis')
 
-    Pcr = load_mult[0]*load
-    print('# eigvals', load_mult)
+    Pcr = load_mult[0]*Nxxunit*circ
+    print('# load_mult', load_mult)
     print('# critical buckling load', Pcr)
 
-    out['P0'] = load
     out['Pcr'] = Pcr
     out['cg_x0'] = cg_x0
     out['eigvals'] = eigvals
@@ -273,13 +300,10 @@ def fkoiter_cyl_SS3(L, R, nx, ny, prop, cg_x0=None, nint=4,
     ua = {}
     for modei in range(koiter_num_modes):
         ua[modei] = eigvecs[:, modei].copy()
-        #if isclose(abs(ua[modei][6::DOF].max()), abs(ua[modei][6::DOF].min())):
-            #ua[modei] /= abs(ua[modei][6::DOF].max())
-        #elif abs(ua[modei][6::DOF].max()) >= abs(ua[modei][6::DOF].min()):
-            #ua[modei] /= ua[modei][6::DOF].max()
-        #else:
-            #ua[modei] /= ua[modei][6::DOF].min()
-        ua[modei] /= ua[modei].max()
+        #NOTE normalizing as Abaqus does, assuming nonzero translations
+        ampl = np.sqrt(ua[modei][0::DOF]**2 + ua[modei][3::DOF]**2 + ua[modei][6::DOF]**2).max()
+        #NOTE using ampl = np.linalg.norm(ua[modei]) does not work
+        ua[modei] /= ampl
         ua[modei] *= havg
 
     phi4 = defaultdict(lambda: 0)

tests/test_koiter_cylinder_newton_raphson.py
@@ -1,47 +1,42 @@
 import sys
 sys.path.append(r'..')
-#sys.path.append(r'../../bfsccylinder')
+sys.path.append(r'../../bfsccylinder')
 
 import numpy as np
 from composites import isotropic_plate, laminated_plate
 
 from bfsccylinder_models.koiter_cylinder_newton_raphson import fkoiter_cyl_SS3
+from bfsccylinder_models.koiter_cylinder_newton_raphson_sanders import fkoiter_cyl_SS3 as fkoiter_cyl_SS3_sanders
 
 def test_Sun_et_al():
-    #Sun, Y., Tian, K., Li, R., and Wang, B., 2020, “Accelerated Koiter Method for Post-Buckling Analysis of Thin-Walled Shells under Axial Compression,” Thin-Walled Struct., 155, p. 106962.
-    L = 0.51 # m
-    R = 0.25 # m
-    ny = 40
-
-    nx = int(ny*L/(2*np.pi*R))
-    if (nx % 2) == 0:
-        nx += 1
-    print('nx, ny', nx, ny)
-
-    E = 72e9
-    nu = 0.31
-    thickness = 0.0005
-    prop = isotropic_plate(E=E, nu=nu, thickness=thickness)
-
-    out = fkoiter_cyl_SS3(L, R, nx, ny, prop, cg_x0=None, nint=4,
-        num_eigvals=2, koiter_num_modes=1)
-    print(out['eigvals'])
-
-
-    print()
+    #Sun, Y., Tian, K., Li, R., and Wang, B., 2020, “Accelerated Koiter Method
+    #for Post-Buckling Analysis of Thin-Walled Shells under Axial Compression,”
+    #Thin-Walled Struct., 155, p. 106962.
+    #Fig. 3 of that paper
+    ny = 50
     R = 0.2032
     L = 0.3556
     nx = int(ny*L/(2*np.pi*R))
     if (nx % 2) == 0:
         nx += 1
     print('nx, ny', nx, ny)
-    laminaprop = (127.629e9, 11.3074e9, 0.300235, 6.00257e9, 6.00257e9, 6.00257e9)
+    E11 = 127.629e9
+    nu12 = 0.300235
+    laminaprop = (E11, 11.3074e9, nu12, 6.00257e9, 6.00257e9, 6.00257e9)
     stack = (+45, -45, 0, 90, 90, 0 -45, +45)
-    plyt = 0.00101539/len(stack)
+    h = 0.00101539
+    plyt = h/len(stack)
+    Nxxunit = 20000.
     prop = laminated_plate(stack=stack, laminaprop=laminaprop, plyt=plyt)
-    out = fkoiter_cyl_SS3(L, R, nx, ny, prop, cg_x0=None, nint=4,
-            num_eigvals=2, koiter_num_modes=1)
+    out = fkoiter_cyl_SS3_sanders(L, R, nx, ny, prop, cg_x0=None, nint=4,
+            num_eigvals=4, koiter_num_modes=1, Nxxunit=Nxxunit, NLprebuck=True)
     print(out['eigvals'])
+    Ncl = E11*h**2/(R*np.sqrt(3*(1-nu12**2)))
+    print('normalized buckling load', out['Pcr']/Ncl)
+    b_1111 = out['koiter']['b_ijkl'][(0, 0, 0, 0)]
+    print('b_1111', b_1111)
+    assert np.isclose(b_1111, -0.041770251378139944)
+
 
 def test_Arbocz_Starnes_2002():
     #Arbocz, J., and Starnes, J. H., 2002, “On a High-Fidelity Hierarchical Approach to Buckling Load Calculations,” New Approaches to Structural Mechanics, Shells and Biological Structures, pp. 271–292.
@@ -62,14 +57,15 @@ def test_Arbocz_Starnes_2002():
     plyt =  0.00101539/len(stack)
     laminaprop = (E11, E22, nu12, G12, G12, G12)
     prop = laminated_plate(stack=stack, laminaprop=laminaprop, plyt=plyt)
-    load = 50000. # N
-    #load = 183366/1.5 # N
+    Nxxunit = 10000. # N
     out = fkoiter_cyl_SS3(L, R, nx, ny, prop, cg_x0=None, nint=4,
-            num_eigvals=2, koiter_num_modes=1, load=load, NLprebuck=True)
+            num_eigvals=2, koiter_num_modes=1, Nxxunit=Nxxunit, NLprebuck=True)
     ref = 391.990772951375*1000 # N/m
-    ans = out['eigvals']*load/2/np.pi/R/ref
+    ans = out['eigvals']*Nxxunit/ref
     print(ans)
-    print(out['koiter']['b_ijkl'])
+    b_1111 = out['koiter']['b_ijkl'][(0, 0, 0, 0)]
+    print('b_1111', b_1111)
+    assert np.isclose(b_1111, -0.8048125534937999)
 
 if __name__ == '__main__':
     test_Arbocz_Starnes_2002()